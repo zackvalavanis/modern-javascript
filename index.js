508dcb958c9164f5e15ac6138bfed20e9e02f78f
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-16 19:40:53 -0500

    updates

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ var moment = require('moment');
 console.log('Hello all!');
 var datetime = moment().format('llll');
 console.log(datetime);
+
+import { sqrt } from 'mathjs/number'
+
+console.log(sqrt(4).toString()) // 2
+console.log(sqrt(-4).toString()) // NaN
+
+
